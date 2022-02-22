d5b279504cb77377088f67d901f0ee8800636849
David Zingg <mail@davidzingg.ch>
2022-02-22 19:24:54 +0100

    - Nicht in endlosloop hängen wenn wifi nicht gefunden wird

--- boot.py
+++ boot.py
@@ -5,7 +5,6 @@ if not sta_if.isconnected():
     print('connecting to network...')
     sta_if.active(True)
     sta_if.connect('Zingg', 'VS5b-HfdQ-d99i-qrqE-lIlr')
-    while not sta_if.isconnected():
-        pass
+
 print('network config:', sta_if.ifconfig())
 webrepl.start()
